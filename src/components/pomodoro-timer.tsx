0487130e40f96fabfb719b06f106863247481757
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-09-12 11:23:25 -0400

    feat: add contactor and button components

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useInterval } from '../hooks/use-interval';
-import { secondsToTime } from '../utils/seconds-to-time';
 import { PomodoroTimerProps } from '../interfaces/pomodoro-timer-props';
+import { Button } from './button.';
+import { Timer } from './timer';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function PomodoroTimer(props: PomodoroTimerProps): JSX.Element {
@@ -10,5 +11,14 @@ export function PomodoroTimer(props: PomodoroTimerProps): JSX.Element {
   useInterval(() => {
     setMainTime(mainTime - 1);
   }, 1000);
-  return <div>Pomodoro Timer {secondsToTime(mainTime)}</div>;
+
+  return (
+    <div className="pomodoro">
+      <h2>You are: working</h2>
+      <Timer mainTime={mainTime} />
+      <Button text="Start" className="button-start" onClick={() => console.log('Start')}></Button>
+      <Button text="Stop" className="button-stop" onClick={() => console.log('Stop')}></Button>
+      <Button text="Restart" className="button-restart" onClick={() => console.log('Restart')}></Button>
+    </div>
+  );
 }

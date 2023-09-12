1d49d3570ccad1af6cd9a3d404fbd216338feaba
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-09-12 09:29:04 -0400

    feat: added pomodoro counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { PomodoroTimer } from './components/pomodoro-timer';
 
 function App(): JSX.Element {
-  return <div className="App">Hello Word</div>;
+  return (
+    <div className="App">
+      <PomodoroTimer defaultPomodoroTimer={120} />
+    </div>
+  );
 }
 
 export default App;

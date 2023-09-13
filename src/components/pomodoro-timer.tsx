89348599b0d94448a982e5cea198d12a332fa4b6
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-09-13 08:25:50 -0400

    feat: add new styling to buttons

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -4,9 +4,8 @@ import { PomodoroTimerProps } from '../interfaces/pomodoro-timer-props';
 import { Button } from './button.';
 import { Timer } from './timer';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function PomodoroTimer(props: PomodoroTimerProps): JSX.Element {
-  const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTimer);
+  const [mainTime, setMainTime] = React.useState(props.PomodoroTime);
 
   useInterval(() => {
     setMainTime(mainTime - 1);
@@ -16,9 +15,18 @@ export function PomodoroTimer(props: PomodoroTimerProps): JSX.Element {
     <div className="pomodoro">
       <h2>You are: working</h2>
       <Timer mainTime={mainTime} />
-      <Button text="Start" className="button-start" onClick={() => console.log('Start')}></Button>
-      <Button text="Stop" className="button-stop" onClick={() => console.log('Stop')}></Button>
-      <Button text="Restart" className="button-restart" onClick={() => console.log('Restart')}></Button>
+      <div className="controls">
+        <Button text="Start" className="button-start" onClick={() => console.log('Start')}></Button>
+        <Button text="Stop" className="button-stop" onClick={() => console.log('Stop')}></Button>
+        <Button text="Restart" className="button-restart" onClick={() => console.log('Restart')}></Button>
+      </div>
+
+      <div className="details">
+        <p>Testando: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, officia.</p>
+        <p>Testando: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, officia.</p>
+        <p>Testando: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, officia.</p>
+        <p>Testando: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, officia.</p>
+      </div>
     </div>
   );
 }
